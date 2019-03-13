17d7c48407c0366e62f41de9cb4483c1c7a2e17c
Haixiang <haixiang6123@gmail.com>
2019-03-13 08:45:35 -0700

    Init Project

---
+++ src/components/Frank.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        Frank
+    </div>
+</template>
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator';
+import HelloWorld from './HelloWrod.vue'
+
+@Component({
+    components: {
+        HelloWorld
+    }
+})
+export default class Frank extends Vue{
+
+}
+</script>
